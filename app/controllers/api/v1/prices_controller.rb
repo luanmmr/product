a3c1071648b14e37d28a47d1bbe5dd89befa7915
Luan Ribeiro <luan.mesquita@hotmail.com>
2021-03-28 03:57:01 -0300

    Refactoring API controllers and API tests

--- app/controllers/api/v1/prices_controller.rb
+++ app/controllers/api/v1/prices_controller.rb
@@ -1,6 +1,43 @@
 class Api::V1::PricesController < Api::V1::ApiController
+  # Rota para todos os preços de um determinado plano que
+  # esteja ativo
+  #
+  # = Exemplo de Request
+  #   GET /api/v1/plans/:plan_id/prices
+  #
+  # = Exemplo de Response
+  #   => Status Code: 200 OK
+  # [
+  #   {
+  #   "id": 1,
+  #   "plan_price": "315.6",
+  #   "plan_id": 2,
+  #   "periodicity_id": 4,
+  #   "created_at": "2021-03-13T20:18:19.746Z",
+  #   "updated_at": "2021-03-13T20:18:19.746Z"
+  #   },
+  #   {
+  #   "id": 2,
+  #   "plan_price": "86.1",
+  #   "plan_id": 2,
+  #   "periodicity_id": 2,
+  #   "created_at": "2021-03-13T20:18:19.808Z",
+  #   "updated_at": "2021-03-13T20:18:19.808Z"
+  #   }
+  # ]
   def index
-    @prices = Plan.activated.find(params[:plan_id]).prices
-    render json: @prices, status: :ok
+    plan = Plan.activated.find_by(id: find_plan.fetch(:plan_id))
+    
+    if plan.present?
+      render json: plan.prices, status: :ok
+    else
+      render body: nil, status: :not_found
+    end
+  end
+
+  private
+
+  def find_plan
+    params.permit(:plan_id)
   end
 end
--- spec/requests/price_spec.rb
+++ spec/requests/price_spec.rb
@@ -1,61 +1,67 @@
 require 'rails_helper'
 
 describe 'Price view API' do
-  context 'index' do
-    it 'render a json with activated plan prices' do
-      plan = create(:plan)
-      monthly = create(:periodicity, name: 'Mensal')
-      quarterly = create(:periodicity, name: 'Trimestral')
-      semiannual = create(:periodicity, name: 'Semestral')
-      price_one = create(:price, plan_price: 20, plan: plan,
-                                 periodicity: monthly)
-      price_other = create(:price, plan_price: 60, plan: plan,
-                                   periodicity: quarterly)
-      price_another = create(:price, plan_price: 120, plan: plan,
-                                     periodicity: semiannual)
+  context '#index' do
+    context 'When a plan is found' do
+      context 'When plan is activated' do
+        context 'When there are recorded prices for the plan' do
+          it 'render a json with all plan prices' do
+            plan = create(:plan)
+            monthly = create(:periodicity, name: 'Mensal')
+            quarterly = create(:periodicity, name: 'Trimestral')
+            semiannual = create(:periodicity, name: 'Semestral')
+            price_one = create(:price, plan_price: 20, plan: plan,
+                                       periodicity: monthly)
+            price_other = create(:price, plan_price: 60, plan: plan,
+                                         periodicity: quarterly)
+            price_another = create(:price, plan_price: 120, plan: plan,
+                                           periodicity: semiannual)
+      
+            get "/api/v1/plans/#{plan.id}/prices"
+            json = JSON.parse(response.body, symbolize_names: true)
+      
+            expect(json[0][:plan_price].to_f).to eq(price_one.plan_price)
+            expect(json[0][:periodicity_id]).to eq(monthly.id)
+            expect(json[1][:plan_price].to_f).to eq(price_other.plan_price)
+            expect(json[1][:periodicity_id]).to eq(quarterly.id)
+            expect(json[2][:plan_price].to_f).to eq(price_another.plan_price)
+            expect(json[2][:periodicity_id]).to eq(semiannual.id)
+          end
+        end
+        context 'Where there are not recorded prices for the plan' do
+          it 'return a empty erray' do
+            plan = create(:plan)
+    
+            get "/api/v1/plans/#{plan.id}/prices"
+            json = JSON.parse(response.body, symbolize_names: true)
+    
+            expect(response).to have_http_status(:ok)
+            expect(json). to eql([])
+          end
+        end
+      end
 
-      get "/api/v1/plans/#{plan.id}/prices"
-      json = JSON.parse(response.body, symbolize_names: true)
-
-      expect(json[0][:plan_price].to_f).to eq(price_one.plan_price)
-      expect(json[0][:periodicity_id]).to eq(monthly.id)
-      expect(json[1][:plan_price].to_f).to eq(price_other.plan_price)
-      expect(json[1][:periodicity_id]).to eq(quarterly.id)
-      expect(json[2][:plan_price].to_f).to eq(price_another.plan_price)
-      expect(json[2][:periodicity_id]).to eq(semiannual.id)
+      context 'When plan is not activated' do
+        it 'return status code 404' do
+          plan = create(:plan, status: :disabled)
+          monthly = create(:periodicity, name: 'Mensal')
+          create(:price, plan_price: 20, plan: plan,
+                         periodicity: monthly)
+    
+          get "/api/v1/plans/#{plan.id}/prices"
+    
+          expect(response).to have_http_status(:not_found)
+          expect(response.body).to eql("")
+        end
+      end
     end
 
-    it 'throw error message when id does not existing' do
-      get '/api/v1/plans/1/prices'
-
-      expect(response).to have_http_status(:not_found)
-      expect(response.body).to include('Couldn\'t find Plan with')
-    end
-
-    it 'return a empty array when there are no prices records for the plan' do
-      plan = create(:plan)
-
-      get "/api/v1/plans/#{plan.id}/prices"
-      json = JSON.parse(response.body, symbolize_names: true)
-
-      expect(response).to have_http_status(:ok)
-      expect(json).to eq([])
-    end
-
-    it 'return a json only with activated plans prices' do
-      product = create(:product_type)
-      plan = create(:plan, name: 'Go', product_type: product, status: :disabled)
-      monthly = create(:periodicity, name: 'Mensal')
-      quarterly = create(:periodicity, name: 'Trimestral')
-      create(:price, plan_price: 20, plan: plan,
-                     periodicity: monthly)
-      create(:price, plan_price: 60, plan: plan,
-                     periodicity: quarterly)
-
-      get "/api/v1/plans/#{plan.id}/prices"
-
-      expect(response).to have_http_status(:not_found)
-      expect(response.body).to include('Couldn\'t find Plan with')
+    context 'When a plan is not found' do
+      it 'return status code 404' do
+        get '/api/v1/plans/1/prices'
+  
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
